74b10269bdce36d12cb4d7535266c2c1e06fc44d
vic-toto <victoriaceccaroni@gmail.com>
2024-12-15 19:14:21 +0100

    added buttons and clicking sensitivity

---
+++ functions.js
@@ -0,0 +1,29 @@
+// functions.js
+
+let box = document.querySelector('.box');
+let isKeyDown = false;
+// Add event listeners
+document.documentElement.addEventListener('mousedown', toggleMouseState);
+document.documentElement.addEventListener('mousemove', handleMouseMove);
+
+function toggleMouseState(event) {
+    // Check if the click occurred outside the cube
+    if (!box.contains(event.target)) {
+        isKeyDown = !isKeyDown; // Toggle the state
+        console.log(`Rotation ${isKeyDown ? 'enabled' : 'disabled'}`);
+    }
+}
+
+// Handles mouse movement and rotates the cube
+function handleMouseMove(e) {
+    if (isKeyDown) {
+        rotateCube(e.clientX, e.clientY);
+    }
+}
+
+// Rotates the cube based on mouse position
+function rotateCube(x, y) {
+    let xValue = Math.floor(x / 2 + 100);
+    let yValue = Math.floor(y / 2 + 100);
+    box.style.transform = `rotateX(${yValue}deg) rotateY(${xValue}deg)`;
+}
